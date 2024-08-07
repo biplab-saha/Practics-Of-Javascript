dbd72685a4016f87d5df028d508d6a94ff032ff0
Biplab Saha <111464784+biplab-saha@users.noreply.github.com>
2024-08-07 10:05:32 +0000

    code add

---
+++ tableOfIntegersNumber.js
@@ -0,0 +1,11 @@
+function tableOfIntegerNumber(valueOfTable) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(valueOfTable * i);
+
+    }
+    return valueOfTable;
+}
+
+let valueOfTable = 23;
+tableOfIntegerNumber(valueOfTable);
+
