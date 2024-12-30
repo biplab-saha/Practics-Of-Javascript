08ee52b04c33b20a9d0334ccb584c124ecbeaabe
Biplab Saha <biplab5523x@gmil.com>
2024-12-31 01:56:50 +0530

    just_change

--- newCheck.js
+++ newCheck.js
@@ -15,5 +15,6 @@ array(69,61,5,8,6,3,48,66,63); // [61, 5, 8, 6, 3, 48, 66]     // 69 is b and re
 
 let i=[];
 let j=[];
-console.log(i==j);  // false
+console.log(i==j); // false     // i and j are two different arrays so they are not equal
+     
 
