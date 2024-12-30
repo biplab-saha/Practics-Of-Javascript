aac92770a0b23155db3e6c5c3247beef9569a4cf
Biplab Saha <biplab5523x@gmil.com>
2024-12-31 01:54:20 +0530

    just_change

---
+++ newCheck.js
@@ -0,0 +1,19 @@
+
+
+
+let a = [6,9,3,1,25,8,7,90];
+
+function array(b, ...a){
+    console.log(a);
+    
+    
+}
+array(69,61,5,8,6,3,48,66,63); // [61, 5, 8, 6, 3, 48, 66]     // 69 is b and rest of the numbers are a
+
+
+
+
+let i=[];
+let j=[];
+console.log(i==j);  // false
+
